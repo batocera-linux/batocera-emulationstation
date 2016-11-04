0a6a484eb236cf8ab99bfe13661db70d9eab88c8
rockaddicted <nirvana_paradize@hotmail.com>
2016-11-04 15:39:41 +0100

    added GuiMsgBoxScroll

---
+++ es-core/src/guis/GuiMsgBoxScroll.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "GuiComponent.h"
+#include "components/NinePatchComponent.h"
+#include "components/ComponentGrid.h"
+#include "resources/Font.h"
+
+class TextComponent;
+class ButtonComponent;
+class ScrollableContainer;
+
+class GuiMsgBoxScroll : public GuiComponent
+{
+public:
+	GuiMsgBoxScroll(Window* window, const std::string& text, 
+		const std::string& name1 = "OK", const std::function<void()>& func1 = nullptr,
+		const std::string& name2 = "", const std::function<void()>& func2 = nullptr, 
+		const std::string& name3 = "", const std::function<void()>& func3 = nullptr,
+		Alignment align = ALIGN_CENTER);
+
+	bool input(InputConfig* config, Input input) override;
+	void onSizeChanged() override;
+	std::vector<HelpPrompt> getHelpPrompts() override;
+
+private:
+	void deleteMeAndCall(const std::function<void()>& func);
+
+	NinePatchComponent mBackground;
+	ComponentGrid mGrid;
+
+
+	std::shared_ptr<TextComponent> mMsg;
+	std::vector< std::shared_ptr<ButtonComponent> > mButtons;
+	std::shared_ptr<ComponentGrid> mButtonGrid;
+	std::function<void()> mAcceleratorFunc;
+	std::shared_ptr<ScrollableContainer> mMsgContainer;
+};
