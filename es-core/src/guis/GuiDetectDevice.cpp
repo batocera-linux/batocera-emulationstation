e59070b2d3793388526fd26d38d0e78b82ed7e2c
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-09-27 22:25:14 +0200

    joystick configuration : fix a crash
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- es-core/src/guis/GuiDetectDevice.cpp
+++ es-core/src/guis/GuiDetectDevice.cpp
@@ -47,8 +47,11 @@ GuiDetectDevice::GuiDetectDevice(Window* window, bool firstRun, const std::funct
 	mMsg1 = std::make_shared<TextComponent>(mWindow, _("HOLD A BUTTON ON YOUR DEVICE TO CONFIGURE IT."), Font::get(FONT_SIZE_SMALL), 0x777777FF, ALIGN_CENTER);
 	mGrid.setEntry(mMsg1, Vector2i(0, 2), false, true);
 
-	const char* msg2str = firstRun ? _("PRESS F4 TO QUIT AT ANY TIME.").c_str() : _("PRESS ESC OR THE HOTKEY TO CANCEL.").c_str();
-	mMsg2 = std::make_shared<TextComponent>(mWindow, msg2str, Font::get(FONT_SIZE_SMALL), 0x777777FF, ALIGN_CENTER);
+	if(firstRun) {
+	  mMsg2 = std::make_shared<TextComponent>(mWindow, _("PRESS F4 TO QUIT AT ANY TIME."), Font::get(FONT_SIZE_SMALL), 0x777777FF, ALIGN_CENTER);
+	} else {
+	  mMsg2 = std::make_shared<TextComponent>(mWindow, _("PRESS ESC OR THE HOTKEY TO CANCEL."), Font::get(FONT_SIZE_SMALL), 0x777777FF, ALIGN_CENTER);
+	}
 	mGrid.setEntry(mMsg2, Vector2i(0, 3), false, true);
 
 	// currently held device
