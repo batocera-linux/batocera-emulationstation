80db1aee588f28a0a7c5fa36ee951ab31d37e4eb
Jérémy <xgenetik57x@gmail.com>
2018-03-05 23:38:03 +0100

    Scan music subfolders
    *Music: check subfolders
    
    Credit : @Supernature2k
    Link : https://gitlab.com/recalbox/recalbox-emulationstation/commit/c9020b2f13d92f75c90351cabe2e75813ab09a20

--- es-core/src/AudioManager.cpp
+++ es-core/src/AudioManager.cpp
@@ -191,8 +191,9 @@ std::vector<std::string> getMusicIn(const std::string &path) {
     const boost::regex my_filter(".*\\.(mp3|ogg)$");
 
 
-    boost::filesystem::directory_iterator end_itr; // Default ctor yields past-the-end
-    for (boost::filesystem::directory_iterator i(target_path); i != end_itr; ++i) {
+    boost::filesystem::recursive_directory_iterator end_itr; // Default ctor yields past-the-end
+    for (boost::filesystem::recursive_directory_iterator i(target_path); i != end_itr; ++i) {
+
         // Skip if not a file
         if (!boost::filesystem::is_regular_file(i->status())) continue;
 
