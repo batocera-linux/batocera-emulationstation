04dfd26aa8e1c41f2cc9bc0c277bef6d39a93bc1
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-05-12 19:23:13 +0200

    minor clock/pad position
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- es-core/src/Window.cpp
+++ es-core/src/Window.cpp
@@ -228,7 +228,7 @@ void Window::update(int deltaTime)
 		// Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
 		// for more information about date/time format
 		strftime(clockBuf, sizeof(clockBuf), "%H:%M", &clockTstruct);
-		mClockText = std::unique_ptr<TextCache>(mDefaultFonts.at(0)->buildTextCache(clockBuf, Renderer::getScreenWidth()-65.0f, Renderer::getScreenHeight()-40.0f, 0x33333366));
+		mClockText = std::unique_ptr<TextCache>(mDefaultFonts.at(0)->buildTextCache(clockBuf, Renderer::getScreenWidth()-75.0f, Renderer::getScreenHeight()-45.0f, 0x33333366));
 	      }
 	      mClockElapsed = 1000; // next update in 1000ms
 	    }
@@ -296,7 +296,7 @@ void Window::render()
 	      padcolor = 0xFF000066;
 	    }
 
-	    Renderer::drawRect((player*(9+4))+2, Renderer::getScreenHeight()-9-2, 9, 9, padcolor);
+	    Renderer::drawRect((player*(10+4))+2, Renderer::getScreenHeight()-10-2, 10, 10, padcolor);
 	  }
 	}
 
