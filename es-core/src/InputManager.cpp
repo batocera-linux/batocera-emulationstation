fe1937702b5976bcdaf15a6a6afac03198719241
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-08-26 19:44:17 +0200

    fix axis against triggers
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- es-core/src/InputManager.cpp
+++ es-core/src/InputManager.cpp
@@ -249,9 +249,10 @@ bool InputManager::parseEvent(const SDL_Event& ev, Window* window)
 	    // in es, the trick is to minus this value to the value to do as if it started at 0
 	    int initialValue = 0;
 	    Sint16 x;
-	    if(SDL_JoystickGetAxisInitialState(mJoysticks[ev.jaxis.which], ev.jaxis.axis, &x)) {
-	      initialValue = x;
-	    }
+	    // SDL_JoystickGetAxisInitialState doesn't work with 8bitdo start+b
+	    //if(SDL_JoystickGetAxisInitialState(mJoysticks[ev.jaxis.which], ev.jaxis.axis, &x)) {
+	    //  initialValue = x;
+	    //}
 
 		//if it switched boundaries
 		if((abs(ev.jaxis.value-initialValue) > DEADZONE) != (abs(mPrevAxisValues[ev.jaxis.which][ev.jaxis.axis]) > DEADZONE)) // batocera
