3d31af4be8e30a1e323b99db0ccc1cf2b933cde9
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-09-28 08:38:48 +0200

    fix: compilation
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- es-app/src/guis/GuiBezelInstallStart.cpp
+++ es-app/src/guis/GuiBezelInstallStart.cpp
@@ -140,7 +140,7 @@ void GuiBezelInstallStart::start(std::string SelectedBezel)
 
 	char trstring[256];
 	snprintf(trstring, 256, _("'%s' ADDED TO DOWNLOAD QUEUE").c_str(), SelectedBezel.c_str()); // batocera
-	mWindow->displayNotificationMessage(_U("\uF019 ") + trstring);
+	mWindow->displayNotificationMessage(_U("\uF019 ") + std::string(trstring));
 
 	ContentInstaller::Enqueue(mWindow, ContentInstaller::CONTENT_THEME, SelectedBezel);	
 	delete this;	
