676eb48cda59a804744f40b0f9e0c314c37c8022
Fabrice CARUSO <fabrice.caruso@free.fr>
2019-11-04 22:07:37 +0100

    Crash fix loading RetroAchievements

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -2087,35 +2087,33 @@ void GuiMenu::openRetroAchievements_batocera()
 	auto s = new GuiSettings(mWindow, _("RETROACHIEVEMENTS").c_str());
 	ComponentListRow row;
 	std::vector<std::string> retroRes = ApiSystem::getInstance()->getRetroAchievements();
-	for (auto it = retroRes.begin(); it != retroRes.end(); it++) 
+	for (auto it = retroRes.begin(); it != retroRes.end(); it++)
 	{
-		int itsize = (*it).length() + 1;
-		char *tmp = new char[itsize];
-		char *lcmsg = new char[itsize];
-		char *longmsg = new char[itsize];
-		std::strcpy(tmp, (*it).c_str());
-		// Format of the string returned by batocera-retroachievements-info:
-		// "Bust-A-Move (SNES)@0 of 27 achievements@0/400 points@Last played 2019-05-20 03:06:16"
-		char *gamename = strtok(tmp, "@"); // or whatever is given before the first @
-		char *achiev = strtok(NULL, "@");
-		char *points = strtok(NULL, "@");
-		char *lastplayed = strtok(NULL, "@");
-		if (achiev)
-			sprintf(lcmsg, "%s ~ %s", gamename, achiev); // beautiful 1-liner
-		else
-			sprintf(lcmsg, "%s", gamename);
-		auto itstring = std::make_shared<TextComponent>(mWindow,
-			lcmsg, ThemeData::getMenuTheme()->Text.font, ThemeData::getMenuTheme()->Text.color);
-		if (points && lastplayed) {
-			sprintf(longmsg, "%s\n%s\n%s\n%s\n", gamename, achiev, points, lastplayed);
+		std::vector<std::string> tokens = Utils::String::split((*it), '@');
+		if (tokens.size() == 0)
+			continue;
+
+		std::string label = tokens[0];
+
+		if (tokens.size() >= 2)
+			label = label + " ~ " + tokens[1];
+
+		auto itstring = std::make_shared<TextComponent>(mWindow, label, ThemeData::getMenuTheme()->Text.font, ThemeData::getMenuTheme()->Text.color);
+
+		if (tokens.size() >= 4)
+		{
+			std::string longmsg = tokens[0] + "\n" + tokens[1] + "\n" + tokens[2] + "\n" + tokens[3];
+
 			row.makeAcceptInputHandler([this, longmsg] {
 				mWindow->pushGui(new GuiMsgBox(mWindow, longmsg, _("OK")));
 			});
 		}
+
 		row.addElement(itstring, true);
 		s->addRow(row);
 		row.elements.clear();
 	}
+
 	mWindow->pushGui(s);
 }
 
