f361a40272fcd234a65d22ff80aee32353a61ccd
Fabrice CARUSO <fabrice.caruso@free.fr>
2020-03-28 10:53:03 +0100

    ImageComponent / VideoComponent : Support for m3u playlist to "rotate" images & videos ( to play images animation )
    #EXTM3U
    #EXT-X-TARGETDURATION:250
    ;#RESETONSHOW
    ;#RANDOM
    neogeo.jpg
    nes.jpg
    ps2.jpg
    psx.jpg

---
+++ es-app/src/Playlists.h
@@ -0,0 +1,53 @@
+#include "components/ImageComponent.h"
+#include <random>
+#include <vector>
+
+class SystemData;
+
+class SystemRandomPlaylist : public IPlaylist
+{
+public:
+	enum PlaylistType
+	{
+		IMAGE,
+		THUMBNAIL,
+		MARQUEE,
+		VIDEO
+	};
+
+	SystemRandomPlaylist(SystemData* system, PlaylistType type);
+	std::string getNextItem() override;
+
+private:
+	SystemData*		mSystem;
+	bool			mFirstRun;
+	PlaylistType	mType;
+
+	std::vector<std::string> mPaths;
+
+	std::random_device	mRandomDevice;
+	std::mt19937		mMt19937;
+	std::uniform_int_distribution<int> mUniformDistribution;
+};
+
+class M3uPlaylist : public IPlaylist
+{
+public:
+	M3uPlaylist(const std::string fileName);
+	std::string getNextItem() override;
+	int getDelay() override;
+	bool getRotateOnShow() override;
+
+private:
+	std::vector<std::string> mPaths;
+
+	int  mPreviousIndex;
+	
+	bool mUseRandom;
+	bool mRotateOnShow;
+	int  mDelay;
+
+	std::random_device	mRandomDevice;
+	std::mt19937		mMt19937;
+	std::uniform_int_distribution<int> mUniformDistribution;
+};
