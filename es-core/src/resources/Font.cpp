0bebfd372133fd73bc2609acb692c0593387326a
Fabrice CARUSO <fabrice.caruso@free.fr>
2020-10-02 20:37:43 +0200

    Fonts : Disable the max font size thing that existed in 5.27

--- es-core/src/resources/Font.cpp
+++ es-core/src/resources/Font.cpp
@@ -78,7 +78,7 @@ size_t Font::getTotalMemUsage()
 Font::Font(int size, const std::string& path) : mSize(size), mPath(path)
 {
 	mSize = size;
-	if(mSize > 160) mSize = 160; // maximize the font size while it is causing issues on linux
+	//if(mSize > 160) mSize = 160; // maximize the font size while it is causing issues on linux
 
 	// GPI
 	if (Renderer::isSmallScreen())
