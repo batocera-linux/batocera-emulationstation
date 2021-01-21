78ced5a330414f07ec296827ffe3ecbb6d5fae6e
Fabrice CARUSO <fabrice.caruso@free.fr>
2021-01-21 19:43:04 +0100

    SaveState manager : Copy file to new slot when running a game, if the users want to reload the saved game in the slot directly from retroarch. Delete it when returning back, if it's the same file.

--- es-app/src/SaveState.cpp
+++ es-app/src/SaveState.cpp
@@ -11,14 +11,23 @@ std::string SaveState::setupSaveState(FileData* game, const std::string& command
 	if (game == nullptr)
 		return command;
 
-	if (!isSlotValid())
-		return command;
-
 	auto path = game->getSourceFileData()->getSystem()->getSaveStateRepository()->getSavesPath();
 	if (path.empty())
 		return command;
-
+	
+	// We start games with new slots : If the users saves the game, we don't loose the previous save
 	int nextSlot = SaveStateRepository::getNextFreeSlot(game);
+
+	if (!isSlotValid())
+	{
+		if (nextSlot > 0)
+		{
+			// We start a game normally but there are saved games : Start game on next free slot to avoid loosing a saved game
+			return command + " --state_slot " + std::to_string(nextSlot);
+		}
+
+		return command;
+	}
 
 	std::string cmd = command;
 	if (slot == -1) // Run current AutoSave
@@ -42,7 +51,6 @@ std::string SaveState::setupSaveState(FileData* game, const std::string& command
 		{
 			Utils::FileSystem::removeFile(autoFilename + ".bak");
 			Utils::FileSystem::renameFile(autoFilename, autoFilename + ".bak");				
-
 		}
 
 		// Copy to state.auto.png file
@@ -51,14 +59,24 @@ std::string SaveState::setupSaveState(FileData* game, const std::string& command
 		{
 			Utils::FileSystem::removeFile(autoImage + ".bak");
 			Utils::FileSystem::renameFile(autoImage, autoImage + ".bak");				
-
 		}
 
 		mAutoImageBackup = autoImage;
 		mAutoFileBackup = autoFilename;
 
 		if (!fileName.empty())
+		{
 			Utils::FileSystem::copyFile(fileName, autoFilename);
+
+			if (nextSlot >= 0)
+			{
+				// Copy file to new slot, if the users want to reload the saved game in the slot directly from retroach
+				mNewSlotFile = Utils::FileSystem::combine(path, Utils::FileSystem::getStem(game->getPath()) + ".state" + std::string(nextSlot == 0 ? "" : std::to_string(nextSlot)));
+				Utils::FileSystem::removeFile(mNewSlotFile);
+				if (Utils::FileSystem::copyFile(fileName, mNewSlotFile))
+					mNewSlotCheckSum = ApiSystem::getInstance()->getMD5(fileName, false);
+			}
+		}
 	}
 	
 	return cmd;
@@ -68,6 +86,14 @@ void SaveState::onGameEnded()
 {
 	if (slot < 0)
 		return;
+
+	if (!mNewSlotCheckSum.empty() && Utils::FileSystem::exists(mNewSlotFile))
+	{
+		// Check if the file in the slot has changed. If it's the same, then delete it & clear the slot
+		auto fileChecksum = ApiSystem::getInstance()->getMD5(mNewSlotFile, false);
+		if (fileChecksum == mNewSlotCheckSum)
+			Utils::FileSystem::removeFile(mNewSlotFile);
+	}
 
 	if (!mAutoFileBackup.empty())
 	{
