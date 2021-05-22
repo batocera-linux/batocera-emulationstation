1a6ce5c4624348a633a925409b8f6a95668a841c
shantigilbert <shantic@gmail.com>
2021-05-22 13:55:53 -0500

    EmuELEC: Cleanup cheevos systems

--- es-app/src/SystemData.cpp
+++ es-app/src/SystemData.cpp
@@ -1819,15 +1819,14 @@ bool SystemData::isCheevosSupported()
 		if (!es_features_loaded)
 		{
 		const std::set<std::string> cheevosSystems = {
-#ifdef _ENABLEEMUELEC
-			"3do", "arcade", "atari2600", "atari7800", "atarilynx", "coleco", "colecovision", "famicom", "fbn", "fbneo", "fds", "gamegear", 
-			"gb", "gba", "gbc", "lynx", "mame", "genesis", "mastersystem", "megadrive", "megadrive-japan", "msx", "n64", "neogeo", "nes", "ngp", 
-			"pcengine", "pcfx", "pokemini", "psx", "saturn", "sega32x", "segacd", "sfc", "sg-1000", "snes", "tg16", "vectrex", "virtualboy", "wonderswan" };
-#else
 				"megadrive", "n64", "snes", "gb", "gba", "gbc", "nes", "fds", "pcengine", "segacd", "sega32x", "mastersystem",
 				"atarilynx", "lynx", "ngp", "gamegear", "pokemini", "atari2600", "fbneo", "fbn", "virtualboy", "pcfx", "tg16", "famicom", "msx1",
 				"psx", "sg-1000", "sg1000", "coleco", "colecovision", "atari7800", "wonderswan", "pc88", "saturn", "3do", "apple2", "neogeo", "arcade", "mame",
-				"nds", "arcade", "atarilynx", "megadrive-japan", "pcenginecd", "supergrafx", "supervision", "snes-msu1" };
+				"nds", "arcade", "atarilynx", "megadrive-japan", "pcenginecd", "supergrafx", "supervision", "snes-msu1" 
+#ifdef _ENABLEEMUELEC 
+                ,"genesis", "msx", "sfc", "vectrex" };
+#else
+                };
 #endif
 
 			if (cheevosSystems.find(getName()) != cheevosSystems.cend())
