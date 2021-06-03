181c69cf3e3680abf6c2b4f0f0c80e2f61eaf993
Fabrice CARUSO <fabrice.caruso@free.fr>
2021-06-03 18:07:16 +0200

    Update : show version information before updating

--- es-app/src/guis/GuiUpdate.cpp
+++ es-app/src/guis/GuiUpdate.cpp
@@ -112,7 +112,12 @@ void GuiUpdate::threadPing()
 	{
 		std::vector<std::string> msgtbl;
 		if (ApiSystem::getInstance()->canUpdate(msgtbl))
+		{
+			if (msgtbl.size() == 1)
+				mUpdateVersion = msgtbl[0];
+
 			onUpdateAvailable();
+		}
 		else
 			onNoUpdateAvailable();
 	}
@@ -154,9 +159,15 @@ void GuiUpdate::update(int deltaTime)
 	switch (mState)
 	{
 		case 1:
-		
+		{
 			mState = 0;
-			window->pushGui(new GuiMsgBox(window, _("REALLY UPDATE?"), _("YES"), [this] 
+
+			std::string message = _("REALLY UPDATE?");
+
+			if (!mUpdateVersion.empty())
+				message = Utils::String::format(_("YOU ARE CURRENTLY USING VERSION : %s\r\nDO YOU WANT TO UPDATE TO VERSION : %s ?").c_str(), ApiSystem::getInstance()->getVersion().c_str(), mUpdateVersion.c_str()),
+
+			window->pushGui(new GuiMsgBox(window, message, _("YES"), [this]
 			{
 				mState = 2;
 				mLoading = true;
@@ -164,9 +175,9 @@ void GuiUpdate::update(int deltaTime)
 				mState = -1;
 				new ThreadedUpdater(mWindow);
 
-			}, _("NO"), [this]  { mState = -1; }));		
-		
-			break;
+			}, _("NO"), [this] { mState = -1; }));
+		}		
+		break;
 
 		case 3:
 		
