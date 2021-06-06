6b177a70fb49f031b46a9331b7d753a688365b30
Fabrice CARUSO <fabrice.caruso@free.fr>
2021-06-06 19:08:28 +0200

    Win32 only : if es-update script last line is a zip file, uncompress it using internal ES installer

--- es-app/src/Win32ApiSystem.cpp
+++ es-app/src/Win32ApiSystem.cpp
@@ -812,6 +812,94 @@ static std::string getScriptPath(const std::string& name)
 	return "";
 }
 
+void Win32ApiSystem::installEmulationStationZip(const std::string& zipFile)
+{
+	std::string path = Utils::FileSystem::getHomePath() + "/.emulationstation/update";
+
+	if (Utils::FileSystem::exists(path))
+		Utils::FileSystem::deleteDirectoryFiles(path);
+	else
+		Utils::FileSystem::createDirectory(path);
+
+	unzipFile(zipFile, path);
+	Utils::FileSystem::removeFile(zipFile);
+
+	auto files = Utils::FileSystem::getDirContent(path, true, true);
+
+	auto pluginFolder = Utils::FileSystem::getExePath() + "/plugins";
+	for (auto pluginFile : Utils::FileSystem::getDirContent(pluginFolder, true))
+	{
+		if (Utils::FileSystem::isDirectory(pluginFile))
+			continue;
+
+		Utils::FileSystem::removeFile(pluginFile + ".old");
+
+		std::string pluginRelativeFile = Utils::FileSystem::createRelativePath(pluginFile, pluginFolder, false);
+		if (Utils::String::startsWith(pluginRelativeFile, "./"))
+			pluginRelativeFile = pluginRelativeFile.substr(2);
+
+		bool existsInArchive = false;
+
+		for (auto installedFile : files)
+		{
+			if (Utils::FileSystem::isDirectory(installedFile))
+				continue;
+
+			std::string relative = Utils::FileSystem::createRelativePath(installedFile, path, false);
+			if (Utils::String::startsWith(relative, "./"))
+				relative = relative.substr(2);
+
+			if (relative == pluginRelativeFile)
+			{
+				existsInArchive = true;
+				break;
+			}
+		}
+
+		if (!existsInArchive)
+		{
+			Utils::FileSystem::removeFile(pluginFile);
+			Utils::FileSystem::renameFile(pluginFile, pluginFile + ".old");
+		}
+	}
+
+	for (auto file : files)
+	{
+		std::string relative = Utils::FileSystem::createRelativePath(file, path, false);
+		if (Utils::String::startsWith(relative, "./"))
+			relative = relative.substr(2);
+
+		std::string localPath = Utils::FileSystem::getExePath() + "/" + relative;
+
+		if (Utils::FileSystem::isDirectory(file))
+		{
+			if (!Utils::FileSystem::exists(localPath))
+				Utils::FileSystem::createDirectory(localPath);
+		}
+		else
+		{
+			// Avoid replacing development exe/lib
+			if ((Utils::String::containsIgnoreCase(localPath, "/RelWithDebInfo/") || Utils::String::containsIgnoreCase(localPath, "/Debug/")) &&
+				(Utils::FileSystem::getExtension(localPath) == ".exe" || Utils::FileSystem::getExtension(localPath) == ".lib"))
+				continue;
+
+			if (Utils::FileSystem::exists(localPath))
+			{
+				Utils::FileSystem::removeFile(localPath + ".old");
+				Utils::FileSystem::renameFile(localPath, localPath + ".old");
+			}
+
+			if (Utils::FileSystem::copyFile(file, localPath))
+			{
+				Utils::FileSystem::removeFile(localPath + ".old");
+				Utils::FileSystem::removeFile(file);
+			}
+		}
+	}
+
+	Utils::FileSystem::deleteDirectoryFiles(path);
+}
+
 std::pair<std::string, int> Win32ApiSystem::updateSystem(const std::function<void(const std::string)>& func)
 {
 	std::string esUpdateScript = getScriptPath("es-update");
@@ -825,17 +913,32 @@ std::pair<std::string, int> Win32ApiSystem::updateSystem(const std::function<voi
 
 		std::string output;
 		auto ret = executeCMD((char*)esUpdateScript.c_str(), output, (char*)esUpdateDirectory.c_str(), func);
-
 		if (ret != 0)
 		{
-			auto lines = Utils::String::split(Utils::String::replace(output, "\n", ""), '\r', true);
+			auto lines = Utils::String::split(Utils::String::replace(output, "\r", ""), '\n', true);
 			if (lines.size() > 0)
-				return std::pair<std::string, int>(lines[lines.size()-1], ret);
+				return std::pair<std::string, int>(lines[lines.size() - 1], ret);
 
 			return std::pair<std::string, int>("error", ret);
 		}
 		else
+		{
+			auto lines = Utils::String::split(Utils::String::replace(output, "\r", ""), '\n', true);
+			if (lines.size() > 0)
+			{			
+				std::string lastLine = Utils::String::trim(Utils::String::replace(lines[lines.size() - 1], "\f", ""));
+				if (!lastLine.find(".zip") != std::string::npos && Utils::FileSystem::exists(lastLine))
+				{
+					if (func != nullptr)
+						func(std::string("Updating EmulationStation"));
+
+					installEmulationStationZip(lastLine);
+					return std::pair<std::string, int>("done.", ret);
+				}
+			}
+
 			return std::pair<std::string, int>("done.", ret);
+		}
 	}
 
 	std::string url = getUrlFromUpdateType(UPDATEURL);
@@ -855,90 +958,13 @@ std::pair<std::string, int> Win32ApiSystem::updateSystem(const std::function<voi
 		if (func != nullptr)
 			func(std::string("Extracting update"));
 
-		unzipFile(zipFile, path);
-		Utils::FileSystem::removeFile(zipFile);
-
-		auto files = Utils::FileSystem::getDirContent(path, true, true);
-
-		auto pluginFolder = Utils::FileSystem::getExePath() + "/plugins";
-		for (auto pluginFile : Utils::FileSystem::getDirContent(pluginFolder, true))
-		{
-			if (Utils::FileSystem::isDirectory(pluginFile))
-				continue;
-
-			Utils::FileSystem::removeFile(pluginFile + ".old");
-
-			std::string pluginRelativeFile = Utils::FileSystem::createRelativePath(pluginFile, pluginFolder, false);
-			if (Utils::String::startsWith(pluginRelativeFile, "./"))
-				pluginRelativeFile = pluginRelativeFile.substr(2);
-
-			bool existsInArchive = false;
-
-			for (auto installedFile : files)
-			{
-				if (Utils::FileSystem::isDirectory(installedFile))
-					continue;
-
-				std::string relative = Utils::FileSystem::createRelativePath(installedFile, path, false);
-				if (Utils::String::startsWith(relative, "./"))
-					relative = relative.substr(2);
-
-				if (relative == pluginRelativeFile)
-				{
-					existsInArchive = true;
-					break;
-				}
-			}
-
-			if (!existsInArchive)
-			{
-				Utils::FileSystem::removeFile(pluginFile);			
-				Utils::FileSystem::renameFile(pluginFile, pluginFile + ".old");
-			}
-		}
-		
-		for (auto file : files)
-		{
-			std::string relative = Utils::FileSystem::createRelativePath(file, path, false);
-			if (Utils::String::startsWith(relative, "./"))
-				relative = relative.substr(2);
-
-			std::string localPath = Utils::FileSystem::getExePath() + "/" + relative;
-
-			if (Utils::FileSystem::isDirectory(file))
-			{
-				if (!Utils::FileSystem::exists(localPath))
-					Utils::FileSystem::createDirectory(localPath);
-			}
-			else
-			{
-				// Avoid replacing development exe/lib
-				if ((Utils::String::containsIgnoreCase(localPath, "/RelWithDebInfo/") || Utils::String::containsIgnoreCase(localPath, "/Debug/")) && 
-					(Utils::FileSystem::getExtension(localPath) == ".exe" || Utils::FileSystem::getExtension(localPath) == ".lib"))
-					continue;
-
-				if (Utils::FileSystem::exists(localPath))
-				{
-					Utils::FileSystem::removeFile(localPath + ".old");
-					Utils::FileSystem::renameFile(localPath, localPath + ".old");
-				}
-
-				if (Utils::FileSystem::copyFile(file, localPath))
-				{
-					Utils::FileSystem::removeFile(localPath + ".old");
-					Utils::FileSystem::removeFile(file);
-				}
-			}
-		}
-
-		Utils::FileSystem::deleteDirectoryFiles(path);
-
+		installEmulationStationZip(zipFile);
 		updateEmulatorLauncher(func);
 
 		return std::pair<std::string, int>("done.", 0);
 	}
 
-	return std::pair<std::string, int>("done.", 0);
+	return std::pair<std::string, int>("error.", 1);
 }
 
 void Win32ApiSystem::updateEmulatorLauncher(const std::function<void(const std::string)>& func)
@@ -1027,7 +1053,11 @@ bool Win32ApiSystem::canUpdate(std::vector<std::string>& output)
 		std::string cmdOutput; 
 		auto ret = executeCMD((char*)esUpdateScript.c_str(), cmdOutput, (char*)esUpdateDirectory.c_str());
 		if (ret == 0 && !cmdOutput.empty())
-			output.push_back(cmdOutput);
+		{
+			auto lines = Utils::String::split(Utils::String::replace(cmdOutput, "\r", ""), '\n', true);
+			if (lines.size() > 0)
+				output.push_back(lines[lines.size() - 1]);
+		}
 
 		return (ret == 0);
 	}
