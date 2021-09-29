37145fa18abe88f63af187a0fd3332f48dede425
Fabrice CARUSO <fabrice.caruso@free.fr>
2021-09-29 20:17:14 +0200

    Dev Option : Add Preload medias option ( off by default ) -> list files in medias (images/videos) folders, just to get OS build filesystem cache -> Less lags when navigating the UI but longest boot time.

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -246,6 +246,7 @@ void Settings::setDefaults()
 	mBoolMap["ThreadedLoading"] = true;
 	mBoolMap["AsyncImages"] = true;
 	mBoolMap["PreloadUI"] = false;
+	mBoolMap["PreloadMedias"] = false;	
 	mBoolMap["OptimizeVRAM"] = true;
 	mBoolMap["OptimizeVideo"] = true;
 
