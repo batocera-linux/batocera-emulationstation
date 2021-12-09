fd4a72ee355cd2b45833e0cdf8ce5cc6b66e5f11
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-12-09 13:03:08 +0100

    no-startup-game option
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -45,6 +45,7 @@
 
 static std::string gPlayVideo;
 static int gPlayVideoDuration = 0;
+static bool enable_startup_game = true;
 
 bool parseArgs(int argc, char* argv[])
 {
@@ -160,6 +161,9 @@ bool parseArgs(int argc, char* argv[])
 		}else if(strcmp(argv[i], "--exit-on-reboot-required") == 0)
 		{
 			Settings::getInstance()->setBool("ExitOnRebootRequired", true);
+		}else if(strcmp(argv[i], "--no-startup-game") == 0)
+		{
+		        enable_startup_game = false;
 		}else if(strcmp(argv[i], "--no-splash") == 0)
 		{
 			Settings::getInstance()->setBool("SplashScreen", false);
@@ -499,8 +503,10 @@ int main(int argc, char* argv[])
 	setLocale(argv[0]);	
 
 #if !WIN32
-	// Run boot game, before Window Create for linux
-	launchStartupGame();
+	if(enable_startup_game) {
+	  // Run boot game, before Window Create for linux
+	  launchStartupGame();
+	}
 #endif
 
 	// metadata init
