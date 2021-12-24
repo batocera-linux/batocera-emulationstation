e1d3daf901e1575a5a94e5e9fb742acb58d8b8ed
Fabrice CARUSO <fabrice.caruso@free.fr>
2021-12-24 11:44:31 +0100

    Refactored Renderer apis to use IRenderer interface. This allow multiple drivers, and set renderer to use with es_settings.cfg file
    For Windows, renderers are :
    <string name="Renderer" value="OPENGL 2.1 / GLSL" /> ( default, used if "Renderer" is not defined )
    or
    <string name="Renderer" value="OPENGL 2.1" /> ( forces the old OpenGL driver )

---
+++ es-core/src/renderers/Renderer_GLES20.h
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef ES_CORE_RENDERER_GLES20_H
+#define ES_CORE_RENDERER_GLES20_H
+
+#if defined(USE_OPENGLES_20) || defined (USE_OPENGL_21)
+
+#define RENDERER_GLES_20
+
+#include "Renderer.h"
+
+namespace Renderer
+{
+	class GLES20Renderer : public IRenderer
+	{
+	public:
+		std::string getDriverName() override;
+		std::vector<std::pair<std::string, std::string>> getDriverInformation() override;
+
+		unsigned int getWindowFlags() override;
+		void         setupWindow() override;
+
+		void         createContext() override;
+		void         destroyContext() override;
+
+		unsigned int createTexture(const Texture::Type _type, const bool _linear, const bool _repeat, const unsigned int _width, const unsigned int _height, void* _data) override;
+		void         destroyTexture(const unsigned int _texture) override;
+		void         updateTexture(const unsigned int _texture, const Texture::Type _type, const unsigned int _x, const unsigned _y, const unsigned int _width, const unsigned int _height, void* _data) override;
+		void         bindTexture(const unsigned int _texture) override;
+
+		void         drawLines(const Vertex* _vertices, const unsigned int _numVertices, const Blend::Factor _srcBlendFactor = Blend::SRC_ALPHA, const Blend::Factor _dstBlendFactor = Blend::ONE_MINUS_SRC_ALPHA) override;
+		void         drawTriangleStrips(const Vertex* _vertices, const unsigned int _numVertices, const Blend::Factor _srcBlendFactor = Blend::SRC_ALPHA, const Blend::Factor _dstBlendFactor = Blend::ONE_MINUS_SRC_ALPHA) override;
+		void		 drawTriangleFan(const Vertex* _vertices, const unsigned int _numVertices, const Blend::Factor _srcBlendFactor = Blend::SRC_ALPHA, const Blend::Factor _dstBlendFactor = Blend::ONE_MINUS_SRC_ALPHA) override;
+
+		void         setProjection(const Transform4x4f& _projection) override;
+		void         setMatrix(const Transform4x4f& _matrix) override;
+		void         setViewport(const Rect& _viewport) override;
+		void         setScissor(const Rect& _scissor) override;
+
+		void         setStencil(const Vertex* _vertices, const unsigned int _numVertices) override;
+		void		 disableStencil() override;
+
+		void         setSwapInterval() override;
+		void         swapBuffers() override;
+	};
+}
+
+#endif // USE_OPENGLES_20
+
+#endif
