57e1304382d5cdc123fa5dfbd3ad54a84f431547
shantigilbert <shantic@gmail.com>
2022-01-20 13:50:23 -0600

    EmuELEC: Enable hostname

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -579,7 +579,7 @@ void GuiMenu::openExternalMounts(Window* mWindow, std::string configName)
         });
        
 		auto emuelec_external_device_retry = std::make_shared< OptionListComponent<std::string> >(mWindow, _("RETRY TIMES"), false);
-		emuelec_external_device_retry->addRange({ { _("AUTO"), "" },{ "1", "1" },{ "2", "2" },{ "3", "3" },{ "4", "4" },{ "5", "5" } }, SystemConf::getInstance()->get("ee_mount.retry"));
+		emuelec_external_device_retry->addRange({ { _("AUTO"), "" },{ "1", "1" },{ "2", "2" },{ "3", "3" },{ "4", "4" },{ "5", "5" },{ "6", "6" },{ "7", "7" },{ "8", "8" },{ "9", "9" },{ "10", "10" },{ "11", "11" },{ "12", "12" },{ "13", "13" },{ "14", "14" },{ "15", "15" },{ "16", "16" },{ "17", "17" },{ "18", "18" },{ "19", "19" },{ "20", "20" },{ "21", "21" },{ "22", "22" },{ "23", "23" },{ "24", "24" },{ "25", "25" },{ "26", "26" },{ "27", "27" },{ "28", "28" },{ "29", "29" },{ "30", "30" } }, SystemConf::getInstance()->get("ee_mount.retry"));
         externalMounts->addWithDescription(_("RETRY TIMES"), _("How many times to retry the mount on boot."), emuelec_external_device_retry);
 		emuelec_external_device_retry->setSelectedChangedCallback([emuelec_external_device_retry](std::string name) { 
             if (SystemConf::getInstance()->set("ee_mount.retry", name)) 
@@ -3811,10 +3811,8 @@ void GuiMenu::openNetworkSettings_batocera(bool selectWifiEnable)
 	s->addGroup(_("SETTINGS"));
 
 #if !WIN32
-#ifndef _ENABLEEMUELEC
 	// Hostname
 	s->addInputTextRow(_("HOSTNAME"), "system.hostname", false);
-#endif
 #endif
 
 	// Wifi enable
--- es-core/src/SystemConf.cpp
+++ es-core/src/SystemConf.cpp
@@ -39,7 +39,11 @@ static std::map<std::string, std::string> defaults =
 	{ "kodi.atstartup", "0" },
 	{ "audio.bgmusic", "1" },
 	{ "wifi.enabled", "0" },
+#ifdef _ENABLEEMUELEC
+	{ "system.hostname", "EMUELEC" },
+#else
 	{ "system.hostname", "BATOCERA" },
+#endif
 	{ "global.retroachievements", "0" },
 	{ "global.retroachievements.hardcore", "0" },
 	{ "global.retroachievements.leaderboards", "0" },
