3780339b55f8ce9ae5522dbd7a3b7822e1b5f213
Lio <lionel.bringuier@gmail.com>
2022-10-07 12:23:30 -0700

    Add Finnish language

--- es-core/src/TextToSpeech.cpp
+++ es-core/src/TextToSpeech.cpp
@@ -182,6 +182,7 @@ void TextToSpeech::setLanguage(const std::string language)
 	if (language_part1 == "es_ES") voice = "spanish";
 	if (language_part1 == "es_MX") voice = "spanish-latin-am";
 	//if(language_part1 == "eu_ES") voice = "";
+	if (language_part1 == "fi_FI") voice = "finnish";
 	if (language_part1 == "fr_FR") voice = "french";
 	if (language_part1 == "hu_HU") voice = "hungarian";
 	if (language_part1 == "it_IT") voice = "italian";
