175f88b8d6278e3ae7cc24aaf91a6ac2d4ddfbd1
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-02-05 22:56:25 +0100

    fix basque name
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -943,7 +943,7 @@ void GuiMenu::openSystemSettings()
 	language_choice->add("ENGLISH (UK)", 	     "en_GB", language == "en_GB");
 	language_choice->add("ESPAÑOL", 	     "es_ES", language == "es_ES" || language == "es");
 	language_choice->add("ESPAÑOL MEXICANO",     "es_MX", language == "es_MX");
-	language_choice->add("BASQUE",               "eu_ES", language == "eu_ES");
+	language_choice->add("EUSKARA",               "eu_ES", language == "eu_ES");
 	language_choice->add("SUOMI",                "fi_FI", language == "fi_FI");
 	language_choice->add("FRANÇAIS",             "fr_FR", language == "fr_FR" || language == "fr");
 	language_choice->add("עברית",                "he_IL", language == "he_IL");
