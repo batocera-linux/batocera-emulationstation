c79e7e5414e37d9940209792b617112d27a92654
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre.pro@gmail.com>
2023-02-09 15:03:28 +0100

    update po
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre.pro@gmail.com>

--- getPoFromWebsite.sh
+++ getPoFromWebsite.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-if test $# -ne 1
+if ! wget -q "http://translations.batocera.org/?q=updatable&type=es" -O - |
+	while read L
+	do
+	    echo "updating ${L}..." >&2
+	    if ! test -d "locale/lang/${L}"
+	    then
+		echo "directory locale/lang/${L} doesn't exist" >&2
+		exit 1
+	    fi
+	    
+	    if ! wget -q "http://translations.batocera.org/po/es_${L}.po" -O "locale/lang/${L}/LC_MESSAGES/emulationstation2.po"
+	    then
+		echo "unable to find file on translations.batocera.org" >&2
+		exit 1
+	    fi
+	done
 then
-    echo "${0} <language>" >&2
+    echo "failed" >&2
     exit 1
 fi
-
-L=$1
-if ! test -d "locale/lang/${L}"
-then
-    echo "directory locale/lang/${L} doesn't exist" >&2
-    exit 1
-fi
-
-if ! wget "http://translations.batocera.org/po/es_${L}.po" -O "locale/lang/${L}/LC_MESSAGES/emulationstation2.po"
-then
-    echo "unable to find file on translations.batocera.org" >&2
-    exit 1
-fi
-
+    
 exit 0
