d011b15d8e22b9cc12eaff5e7d85cd974ab78f9a
stosman <stos.man@gmail.com>
2023-03-29 19:04:17 +0100

    modified:   es-core/src/utils/TimeUtil.cpp to include days in the secondstostring function

--- es-core/src/utils/TimeUtil.cpp
+++ es-core/src/utils/TimeUtil.cpp
@@ -318,11 +318,27 @@ namespace Utils
 
 			char buf[256];
 
-			int h = 0, m = 0, s = 0;
+			int d =0, h = 0, m = 0, s = 0;
+			d = seconds / 86400;
 			h = (seconds / 3600) % 24;
 			m = (seconds / 60) % 60;
 			s = seconds % 60;
-			
+			if (d > 0)
+			{
+				snprintf(buf, 256, _("%d d").c_str(), d);
+				if (h > 0)
+				{
+					std::string days(buf);
+					snprintf(buf, 256, _("%d h").c_str(), h);
+					if(m > 0)
+					{
+						std::string hours(buf);
+						snprintf(buf, 256, _("%d mn").c_str(), m);
+						return days + " " + hours + " " + std::string(buf);
+					}
+					return days + " " + std::string(buf);
+				}
+			}
 			if (h > 0)
 			{
 				snprintf(buf, 256, _("%d h").c_str(), h);
