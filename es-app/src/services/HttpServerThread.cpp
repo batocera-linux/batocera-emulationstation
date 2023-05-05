cd143167dce85ae5348e26d19326444f7a0943e0
NullPopPo <sylph@reset.jp>
2023-05-05 10:10:43 +0000

    runningGame return empty json when not running (status back to 201)

--- es-app/src/services/HttpServerThread.cpp
+++ es-app/src/services/HttpServerThread.cpp
@@ -266,7 +266,7 @@ void HttpServerThread::run()
 		std::string ret = HttpApi::getRunnningGameInfo();
 		if (ret.empty())
 		{
-			res.set_content("201 NO GAME RUNNING", "text/html");
+			res.set_content("{}", "application/json");
 			res.status = 201;
 		}
 		else
