d1e728c6c62f19cc10d591001b44d290982918c4
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-12-30 11:27:28 +0100

    guns/wheels resource file checker
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ resources/checkWheelGunGamesResources.py
@@ -0,0 +1,39 @@
+
+import xml.etree.ElementTree as ET
+
+
+def xml2systemsArrays(path):
+    res = dict()
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    for nodesystem in root.findall(".//system"):
+        system = nodesystem.get("name")
+        if system in res:
+            raise Exception("duplicated system found")
+        res[system] = []
+        for nodegame in nodesystem:
+            res[system].append(nodegame.text)
+    return res
+
+def checkSystemGames(system, games):
+    hasError = False
+    for x in range(len(games)): # for each game, we check it is not a part of the next ones
+        for y in range(x+1, len(games)):
+            if games[x] in games[y]:
+                if games[x] == games[y]:
+                    print("system {} failed : duplicated game {}".format(system, games[x]))
+                else:
+                    print("system {} failed : game {} should be before game {} in the list".format(system, games[y], games[x]))
+                hasError = True
+    return hasError
+
+for file in ["gungames.xml", "wheelgames.xml"]:
+    systems = xml2systemsArrays(file)
+    hasError = False
+    for system in systems:
+        if checkSystemGames(system, systems[system]) == False:
+            hasError = True
+
+if hasError:
+    raise Exception("checking systems failed")
