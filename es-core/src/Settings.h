528a3e4d2207264ae9be90651226f947e4c963a4
Fabrice CARUSO <fabrice.caruso@free.fr>
2024-12-14 16:16:05 +0100

    Add new option : DON'T SHOW GROUPS WITH ONY ONE SYSTEM

--- es-core/src/Settings.h
+++ es-core/src/Settings.h
@@ -99,6 +99,7 @@ public:
 	DEFINE_BOOL_SETTING(NetPlayShowOnlyRelayServerGames)
 	DEFINE_BOOL_SETTING(NetPlayShowMissingGames)			
 	DEFINE_BOOL_SETTING(LoadEmptySystems)		
+	DEFINE_BOOL_SETTING(HideUniqueGroups)
 	DEFINE_STRING_SETTING(HiddenSystems)
 	DEFINE_STRING_SETTING(TransitionStyle)
 	DEFINE_STRING_SETTING(GameTransitionStyle)		
