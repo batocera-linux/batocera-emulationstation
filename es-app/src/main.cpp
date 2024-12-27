86746bc8db430796f9e28466bcd49824c2de562f
Fabrice CARUSO <fabrice.caruso@free.fr>
2024-12-27 12:33:25 +0100

    [Cheevos token] Use WatchersManager to check for Cheevos token instead of main() function

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -620,34 +620,6 @@ int main(int argc, char* argv[])
 
 	window.closeSplashScreen();
 
-	bool cheevosState = SystemConf::getInstance()->getBool("global.retroachievements");
-	std::string cheevosUsername = SystemConf::getInstance()->get("global.retroachievements.username");
-	std::string cheevosPassword = SystemConf::getInstance()->get("global.retroachievements.password");
-	if (cheevosState && !(cheevosUsername.empty() || cheevosPassword.empty()))
-	{
-		auto cheevosTokenThread = [cheevosUsername, cheevosPassword]() {
-			std::string tokenOrError;
-			if (RetroAchievements::testAccount(cheevosUsername, cheevosPassword, tokenOrError))
-			{
-				if (tokenOrError == SystemConf::getInstance()->get("global.retroachievements.token"))
-				{
-					LOG(LogInfo) << "Cheevos token is unchanged.";
-				}
-				else
-				{
-					LOG(LogInfo) << "Generated a new cheevos token, saving.";
-					SystemConf::getInstance()->set("global.retroachievements.token", tokenOrError);
-					SystemConf::getInstance()->saveSystemConf();
-				}
-			}
-			else
-			{
-				LOG(LogError) << "Failed to generate a new cheevos token: " << tokenOrError;
-			}
-		};
-		std::thread(cheevosTokenThread).detach();
-	}
-
 	// Create a flag in  temporary directory to signal READY state
 	ApiSystem::getInstance()->setReadyFlag();
 
