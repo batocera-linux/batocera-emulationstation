b0ddf72f3e28d03240bde6761441ce7edbc5f563
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-12-05 00:57:34 +0100

    removed unwanted changes

--- build_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# make the image, you can comment this after
-# docker build --platform linux/arm64 --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t batocera-build-arm64 .
-
-# build the project
-docker run --rm --platform linux/arm64 -v "${PWD}:/build" batocera-build-arm64 cmake .
-docker run --rm --platform linux/arm64 -v "${PWD}:/build" batocera-build-arm64 make -j6
