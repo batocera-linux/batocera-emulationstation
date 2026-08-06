ee78178bf13c1ef51748ec6f1e2a803be2b7cfe5
Nota Inutilis <9675966+NotaInutilis@users.noreply.github.com>
2026-08-06 19:27:52 +0200

    Fixing a typo in UI: ccp and po files
    Spacing is my spassion

--- es-core/src/guis/GuiTextEditPopupKeyboard.cpp
+++ es-core/src/guis/GuiTextEditPopupKeyboard.cpp
@@ -206,7 +206,7 @@ GuiTextEditPopupKeyboard::GuiTextEditPopupKeyboard(Window* window, const std::st
 				if (lower == "SHIFT")
 				{
 					// Special case for shift key
-					mShiftButton = std::make_shared<ButtonComponent>(mWindow, _U("\uF176"), _("SHIFTS FOR UPPER,LOWER, AND SPECIAL"), [this] { shiftKeys(); }, false);					
+					mShiftButton = std::make_shared<ButtonComponent>(mWindow, _U("\uF176"), _("SHIFTS FOR UPPER, LOWER, AND SPECIAL"), [this] { shiftKeys(); }, false);					
 					button = mShiftButton;
 				}
 				else if (lower == "ALT")
